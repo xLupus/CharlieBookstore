6f3ecd39a1c348eb7a09bcc9eac3405caf25fcfb
VINICIUS DO AMARAL SOUZA <vinicius.asouza34@senacsp.edu.br>
2022-09-21 22:36:06 -0300

    Pagina de produto Finalizada

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\CategoriaController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProdutoController::class, 'home'])->name('home');
+Route::resource('/produto', ProdutoController::class);
+Route::get('/categoria/{categoria}', [CategoriaController::class, 'show']);
