5ca037b3a2ebb644ddc0ef9552d09f5f52b34ab9
VINICIUS DO AMARAL SOUZA <vinicius.asouza34@senacsp.edu.br>
2022-09-22 20:34:44 -0300

    Comportamento de imagem

---
+++ public/js/produto.js
@@ -0,0 +1,8 @@
+let pictures    = document.querySelectorAll('.book-pictures');
+let picture     = document.getElementById('book-picture');
+
+for (let i = 0; i < pictures.length; i++) {
+    pictures[i].addEventListener('click', function() {
+        picture.src = pictures[i].src
+    })
+}
