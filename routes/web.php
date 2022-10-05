f6a7b2234900491fdf1c04546c1fa293e80849a8
RAFAEL MARTINS SOARES <rafael.msoares5@senacsp.edu.br>
2022-10-05 20:18:55 -0300

    subindo

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\CategoriaController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+*/
 Route::get('/', [ProdutoController::class, 'index'])->name('home');
 Route::resource('/produto', ProdutoController::class);
 Route::get('/categoria/{categoria}', [CategoriaController::class, 'show']);
+
+require __DIR__.'/auth.php';
