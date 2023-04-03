8c2287ee06370d37a0e47bfe8c48508d8653784d
Vinicius <vinicius.am.souza221b@gmail.com>
2023-04-03 17:15:03 -0300

    Adaptando projeto para local

--- resources/views/carrinho/index.blade.php
+++ resources/views/carrinho/index.blade.php
@@ -180,7 +180,7 @@
                                     </form>
                                 </div>
                                 <div class="col-2 bg-light rounded-top">
-                                    <button type="button" class="btn btn-default p-0 border border-0 float-end">
+                                    <button type="button" class="btn btn-default p-0 border-0 float-end">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
                                             <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                                         </svg>
