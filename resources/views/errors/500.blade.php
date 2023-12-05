83b8eadc8d3b9958236d4420cdc1944b84ba208f
Vinicius <vinicius.am.souza221b@gmail.com>
2023-12-05 19:36:03 -0300

    Configurando tela de erro 500

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,5 @@
-@extends('errors::minimal')
+@extends('errors::layout')
 
-@section('title', __('Server Error'))
+@section('title', __('Erro de Servidor'))
 @section('code', '500')
-@section('message', __('Server Error'))
+@section('message', __('Erro de Servidor'))
